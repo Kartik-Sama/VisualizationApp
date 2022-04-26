946e98e3ca3abc2e0c4124d01b224a265ba43a53
Kartik-Sama <sai.karthik@iiitb.ac.in>
2022-04-26 10:17:52 +0530

    Made colours consitent

--- static/js/survey.js
+++ static/js/survey.js
@@ -148,38 +148,6 @@ function d3heatMap(data) {
     .range(["white", "#69b3a2"])
     .domain([0,1])
 
-    // //Read the data
-    // d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
-
-    // // create a tooltip
-    // var tooltip = d3.select(panelId)
-    // .append("div")
-    // .style("opacity", 0)
-    // .attr("class", "tooltip")
-    // .style("background-color", "white")
-    // .style("border", "solid")
-    // .style("border-width", "2px")
-    // .style("border-radius", "5px")
-    // .style("padding", "5px")
-
-    // // Three function that change the tooltip when user hover / move / leave a cell
-    // var mouseover = function(d) {
-    // tooltip.style("opacity", 1)
-    // }
-    // var mousemove = function(d) {
-    // tooltip
-    //     .html("The exact value of<br>this cell is: " + d.value)
-    //     .style("left", (d3.mouse(this)[0]+70) + "px")
-    //     .style("top", (d3.mouse(this)[1]) + "px")
-    // }
-    // var mouseleave = function(d) {
-    // tooltip.style("opacity", 0)
-    // }
-
-    // // add the squares
-    // svg.selectAll()
-    // .data(data, function(d) {return d.group+':'+d.variable;})
-    // .enter()
     const cellsize = x.bandwidth();
     for([ind, val] of dispData.entries()) {
         svg.append("rect")
